348e0097c7fa5bcaf7b1238ae8a1e4fb8e0be6f2
tembdb-hes <145921610+tembdb-hes@users.noreply.github.com>
2025-08-29 14:45:31 -0500

    Update workspace files

--- local_proxy_auth.bash
+++ local_proxy_auth.bash
@@ -1,4 +1,6 @@
 #Install gcloud sdk and run this script
 ## to connect from a local machine to the Cloud Run Server.
 ## Use the provided url and port in the mcp.json file (or similar), as shown in the .vscode folder if using VSCODE as your MCP Client
-gcloud run services proxy mcp-server --region=us-central1
+export SERVER_NAME=<SERVER_NAME> #same as in gcloud_infra.bash
+export REGION=<region> #same as in gcloud_infra.bash
+gcloud run services proxy $SERVER_NAME --region=$REGION
